4b2d290d54497d32008c6c45594073385760327a
Isaac <76838845+isaxk@users.noreply.github.com>
2024-11-30 09:05:26 +0000

    new api key

--- src/lib/supabase/init.ts
+++ src/lib/supabase/init.ts
@@ -1,9 +1,7 @@
+import { SUPABASE_ANON_KEY } from '$env/static/private';
 import type { Database } from '$lib/types/supabase';
 import { createClient } from '@supabase/supabase-js';
 
 
 const supabaseUrl = 'https://cexwrecqxupsbtcxapip.supabase.co';
-const supabaseKey =
-	'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNleHdyZWNxeHVwc2J0Y3hhcGlwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzI3MjYyNTEsImV4cCI6MjA0ODMwMjI1MX0.JHONIaNvXH1pZfjD8mtuGqQoMbU3ElAWpJoQQu8nYtk';
-    
-export const supabase = createClient<Database>(supabaseUrl, supabaseKey);
+export const supabase = createClient<Database>(supabaseUrl, SUPABASE_ANON_KEY);
